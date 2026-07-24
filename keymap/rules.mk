8a5b609129005d3df74c0ab5de4aec36ed1b32ef
[object Object] <diego.peralta.dev@gmail.com>
2026-07-24 15:24:58 +0200

    fixes

--- keymap/rules.mk
+++ keymap/rules.mk
@@ -1,3 +1,5 @@
+# Voyager Dev-ES keymap features
+
 CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
 MOUSEKEY_ENABLE = no
@@ -5,6 +7,11 @@ MOUSEKEY_ENABLE = no
 CAPS_WORD_ENABLE = yes
 COMBO_ENABLE = yes
 TAP_DANCE_ENABLE = no
-
 SPACE_CADET_ENABLE = no
-ORYX_ENABLE = no
+
+# Required by ZSA's Voyager firmware code.
+ORYX_ENABLE = yes
+
+# Do not enable this for this standalone keymap.
+# It expects an Oryx-generated rgb_matrix_kb.inc file.
+# RGB_MATRIX_CUSTOM_KB = yes
